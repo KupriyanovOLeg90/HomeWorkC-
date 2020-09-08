696e100e3afe4c1a898680ad77701eaf3da8e968
okupriyanov <okupriyanov@neoservice.kz>
2020-09-08 09:28:57 +0600

    Домашнее задание Перегрузка операторов и методов Цель: Сформулировать сущность, создать класс с операторами над ней. Придумать логически обоснованные операторы для управления сущностями. Написать небольшой текст на русском/английском языке с описанием вашей сущности и операторов. Реализовать все необходимые конструкторы, методы, операторы и написать программу для демонстрации. Критерии оценки: +2 Есть описание класса и операрторов на русском/английском языке +1 Есть класс. +1 Есть конструктор. +1 Есть методы. +1 Есть операторы. +1 Есть перегруженные операторы. +1 Создан индексатор. +1 Создан метод расширения для класса string. +1 Написана программа для тестирования/демонстрации класса.

---
+++ HomeWorkC№/FirstLesson/Classes/Rectangle.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq.Expressions;
+
+namespace FirstLesson.Classes
+{
+    /// <summary>
+    /// Класс описывает Прямоугольник
+    /// </summary>
+    class Rectangle
+    {
+        private int Length { get; set; }
+        private int Height { get; set; }
+
+        public Rectangle(int length, int height)
+        {
+            Length = length;
+            Height = height;
+        }
+
+        /// <summary>
+        /// Подсчет площади
+        /// </summary>
+        /// <returns>Площадь</returns>
+        public int Area()
+        {
+            return Length * Height;
+        }
+
+        /// <summary>
+        /// Подсчет периметра
+        /// </summary>
+        /// <returns>Периметр</returns>
+        public int Perimeter()
+        {
+            return Length * 2 + Height * 2;
+        }
+
+        /// <summary>
+        /// Прибавляет к текущему прямоугольнику другой прямоугольник
+        /// </summary>
+        /// <param name="rectangle">Прямоугольник который будем прибавлять</param>
+        public void Add(Rectangle rectangle)
+        {
+            Length += rectangle.Length;
+            Height += rectangle.Height;
+        }
+
+        /// <summary>
+        /// Прибавляет к текущемму прямоугольнику длину и высоту
+        /// </summary>
+        /// <param name="length">Длина</param>
+        /// <param name="height">Высота</param>
+        public void Add(int length, int height)
+        {
+            Length += length;
+            Height += height;
+        }
+
+        public int this[string side]
+        {
+            get
+            {
+                switch (side.ToLower())
+                {
+                    case "length":
+                        return Length;
+                        break;
+                    case "height":
+                        return Height;
+                        break;
+                    default: 
+                         throw new IndexOutOfRangeException();
+                }
+            }
+        }
+
+        public static Rectangle operator +(Rectangle rectangle1, Rectangle rectangle2)
+        {
+            return new Rectangle(rectangle1.Length + rectangle2.Length, rectangle1.Height + rectangle2.Height);
+        }
+       
+        /// <summary>
+        /// Увеличивает прямогольник на заданную величику
+        /// </summary>
+        /// <param name="rectangle"> Прямоугольник который будет увеличен</param>
+        /// <param name="val">Значение на сколько будет увеличен прямоугольник</param>
+        /// <returns></returns>
+        public static Rectangle operator +(Rectangle rectangle, int val)
+        {
+            return new Rectangle(rectangle.Length + val, rectangle.Height + val);
+        }
+
+        public static bool operator ==(Rectangle rectangle1, Rectangle rectangle2)
+        {
+            return rectangle1?.Length == rectangle2?.Length && rectangle1?.Height == rectangle2?.Height;
+        }
+
+        public static bool operator !=(Rectangle rectangle1, Rectangle rectangle2)
+        {
+            return rectangle1?.Length != rectangle2?.Length && rectangle1?.Height != rectangle2?.Height;
+        }
+
+
+        public override string ToString()
+        {
+            return $"Прямоугольник со сторонами: длинна {Length} и ширина {Height}";
+        }
+    }
+}
---
+++ HomeWorkC№/FirstLesson/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using FirstLesson.Classes;
+
+namespace FirstLesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Rectangle rectangle = new Rectangle(10,20);
+            Console.WriteLine($"Площадь: {rectangle.Area()}");
+            Console.WriteLine($"Периметр: {rectangle.Perimeter()}");
+
+
+            rectangle.Add(5,7);
+            Console.WriteLine("Прибавил значения");
+            Console.WriteLine($"Площадь: {rectangle.Area()}");
+            Console.WriteLine($"Периметр: {rectangle.Perimeter()}");
+
+            Console.WriteLine("Новыйй прямоугольник");
+
+            Rectangle rectangle2 = new Rectangle(35, 10);
+            Console.WriteLine($"Площадь: {rectangle2.Area()}");
+            Console.WriteLine($"Периметр: {rectangle2.Perimeter()}");
+
+
+            rectangle2.Add(rectangle);
+            Console.WriteLine("добавил первый прямоугольник ко второму");
+            Console.WriteLine($"Площадь: {rectangle2.Area()}");
+            Console.WriteLine($"Периметр: {rectangle2.Perimeter()}");
+
+            var sumRectangle = rectangle + rectangle2;
+            Console.WriteLine("Сложил прямоугольники оператором");
+            Console.WriteLine($"Площадь: {sumRectangle.Area()}");
+            Console.WriteLine($"Периметр: {sumRectangle.Perimeter()}");
+
+
+            Console.WriteLine($"Длина: {sumRectangle["length"]} ");
+            Console.WriteLine($"Высота: {sumRectangle["height"]}");
+
+            Console.WriteLine();
+
+
+            Console.ReadKey();
+        }
+    }
+}
